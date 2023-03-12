0089df3e1041f85650e315c37d2b574f1e536631
ukeysandeep <126758440+ukeysandeep@users.noreply.github.com>
2023-03-12 23:19:07 +0530

    Update file1.py
    poll scm testing 1

--- file1.py
+++ file1.py
@@ -1,19 +1,4 @@
 This is first file 
 update 0
 update 1
-update for poll scm
-second update for poll scm
-for web hook testing
-web hook testing  
-cross checking this option
-poll scm testing
-testing 1000
-update on date 8/3/2023 2:45 
-web hook updated in github repository
-second line after updateing web hook
-testing again
-poll scm testing 
-poll scm testing 2
-new job is created for poll scm testing
-git pull command
-git pull comamnd 2nd time
+update for poll scm test 1
